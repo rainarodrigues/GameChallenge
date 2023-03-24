9cecdaff5308973495943f1db19abe6e6177d27c
rainarodrigues <rainarrodrigues@gmail.com>
2023-03-24 10:26:46 -0300

    Initial Commit

---
+++ GameChallenge/GameChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameChallengeApp.swift
+//  GameChallenge
+//
+//  Created by Raina Rodrigues de Lima on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GameChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
