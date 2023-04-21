8c520f307ff7b0b4108d6e5d885c3c1c0d2f9fc4
Pedro Muniz <phgmuniz@gmail.com>
2023-04-21 20:49:28 -0300

    Inicio da criacao da tela de detalhes das const

---
+++ WitchGame/Models/ConstellationDetailsModel.swift
@@ -0,0 +1,20 @@
+//
+//  ConstellationDetailsModel.swift
+//  WitchGame
+//
+//  Created by Pedro Muniz on 20/04/23.
+//
+
+import Foundation
+
+struct ConstellationDetailsModel {
+    let id: Int
+    let constellationName: String
+    let constellationImage: String
+    let animalImage: String
+    let constellationDetails: String
+    let position: String
+    let season: String
+    let trivia: String
+    let record: Int
+}
--- WitchGame/Scenes/POC/PuzzleGameView.swift
+++ WitchGame/Scenes/POC/PuzzleGameView.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 import SpriteKit
 
-struct PuzzleGameView: View {
-    var scene: SKScene {
-        let scene = GameScene()
-        scene.size = CGSize(width: 400, height: 400)
-        scene.scaleMode = .fill
-        
-        return scene
-    }
-    
-    var body: some View {
-        SpriteView(scene: scene)
-            .frame(width: 600, height: 300)
-            .ignoresSafeArea()
-    }
-}
-
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        PuzzleGameView()
-            .previewInterfaceOrientation(.landscapeRight)
-    }
-}
+//struct PuzzleGameView: View {
+//    var scene: SKScene {
+//        let scene = GameScene()
+//        scene.size = CGSize(width: 400, height: 400)
+//        scene.scaleMode = .fill
+//
+//        return scene
+//    }
+//
+//    var body: some View {
+//        SpriteView(scene: scene)
+//            .frame(width: 600, height: 300)
+//            .ignoresSafeArea()
+//    }
+//}
+//
+//struct SwiftUIView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PuzzleGameView()
+//            .previewInterfaceOrientation(.landscapeRight)
+//    }
+//}
