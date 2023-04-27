89ddde5de716b0fac92c2ba9579691ba17c29b72
maahdisousa <mmdisousa@gmail.com>
2023-04-27 08:34:26 -0300

    nome tela inicial

--- WitchGame/Views/SplashView.swift
+++ WitchGame/Views/SplashView.swift
@@ -11,11 +11,13 @@ import Lottie
 struct SplashView: View {
     @State var isActive: Bool = false
     let animationWitchView = LottieAnimationView()
-
+    
+    @State var animatedValue = 1.0
+    
     @State var rotation = 0.0
     @State var scaleXY = 1.0
     @State var positionX = -150
-
+    
     var body: some View {
         ZStack() {
             if self.isActive {
@@ -23,39 +25,41 @@ struct SplashView: View {
             } else {
                 LottieBackground(lottieName: "starsbg")
                     .ignoresSafeArea(.all)
-                ZStack {
-                    Image("WitchFlyingStatic")
-                        .resizable()
-                        .frame(width: 220, height: 219.15)
-// We can have a flipping in the air effect if the line below is uncommented
-//                        .rotationEffect(.degrees(rotation))
-//                        .scaleEffect(CGFloat(scaleXY))
-                        .offset(x: CGFloat(positionX))
-                        .animation(Animation.easeInOut(duration: 1).speed(0.5), value: rotation)
-//                    LottieBackground(lottieName: "witch-animation")
-//                        .frame(width: 350, height: 350)
-                        .onAppear() {
-                            rotation += 360
-                            scaleXY += 1.1
-                            positionX += 350
-                        }
-                }
+                HStack {
+                    VStack {
+                        Image("AppTitleProvisory2")
+                            .resizable()
+                            .scaledToFill()
+//                            .scaleEffect(animatedValue)
+                            .frame(width: 180.0, height:100.0)
+                            .position(x: 310, y: 200)
+                            .opacity(0.50 * animatedValue)
+                            .animation(.easeOut.delay(1.5), value: animatedValue)
+                        
+                        Image("WitchFlyingStatic")
+                            .resizable()
+                            .frame(width: 230, height: 229.15)
+                            .offset(x: CGFloat(positionX))
+                            .position(x: 75)
+                            .animation(Animation.easeInOut(duration: 1).speed(0.5), value: rotation)
+                            .onAppear() {
+                                rotation += 360
+                                scaleXY += 1.1
+                                positionX += 675
+                            }
+                        
+                    } .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }     .onAppear() {
+                    animatedValue += 1
+            }
+                
+                //            struct SplashView_Previews: PreviewProvider {
+                //                static var previews: some View {
+                //                    SplashView()
+                //                        .previewInterfaceOrientation(.landscapeRight)
+                //                }
+                //            }
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .onAppear() {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                withAnimation {
-                    self.isActive = true
-                }
-            }
-        }
-    }
-}
-
-struct SplashView_Previews: PreviewProvider {
-    static var previews: some View {
-        SplashView()
-            .previewInterfaceOrientation(.landscapeRight)
     }
 }
